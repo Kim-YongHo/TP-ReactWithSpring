b3f738eec3cee87b881fc1b23303946403b22c34
YongHo_Kim <ashore92@naver.com>
2024-08-01 10:28:17 +0900

    240801-설정

---
+++ rws-api-mall/rws-api-mall/src/main/java/com/rws/mall/RwsApiMallApplication.java
@@ -0,0 +1,14 @@
+package com.rws.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsApiMallApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RwsApiMallApplication.class, args);
+    }
+
+}
---
+++ rws-api-mall/rws-api-mall/src/test/java/com/rws/mall/RwsApiMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rws.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwsApiMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
